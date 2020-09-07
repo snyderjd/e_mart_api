d9ccba44fb9e4418e71578fa8f18c1d5c563b210
Joe Snyder <snyderjosephd@yahoo.com>
2020-09-07 15:32:10 -0500

    ProductSerializer returns product's quantity and category_id

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :category, :price, :is_active, :image, :image_url
+  attributes :id, :name, :description, :quantity, :category_id, :category, :price, :is_active, :image, :image_url
 end
